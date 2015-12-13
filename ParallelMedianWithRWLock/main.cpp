4817f1e533dae4e14605b07c626d168129718c26
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-12-13 21:26:46 +0300

    Initial Commit

---
+++ ParallelMedianWithRWLock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ParallelMedianWithRWLock
+//
+//  Created by Руслан on 13.12.15.
+//  Copyright © 2015 MIPT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
